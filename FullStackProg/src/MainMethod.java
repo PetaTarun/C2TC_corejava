30fd15d6dc38c747f55e05550dadd75016b3ac78
tarun <tarun@DESKTOP-83VNE9M>
2023-09-29 19:30:31 +0530

    first commit

---
+++ FullStackProg/src/MainMethod.java
@@ -0,0 +1,12 @@
+
+public class MainMethod {
+
+	public static void main(String[] args) {
+ //public is used for visible to all STATIC is used for without creation of obj,main is method,args are dynamic array.
+		System.out.println("welcome");
+	}
+	
+}
+	
+
+
