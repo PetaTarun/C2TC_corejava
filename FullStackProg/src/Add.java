b15b38334e7bd3dc3785dfbdad7e2ff89e6f43eb
tarun <tarun@DESKTOP-83VNE9M>
2023-10-05 18:42:32 +0530

    secound commit

---
+++ FullStackProg/src/Add.java
@@ -0,0 +1,13 @@
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=2;
+		int b=5;
+		int c=a+b;
+		System.out.println("addation is"+c);
+
+	}
+
+}
