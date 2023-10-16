9864969d57aef3de1ecabfe6db8b9585163a2537
tarun <tarun@DESKTOP-83VNE9M>
2023-10-16 10:36:34 +0530

    commit6

---
+++ FullStackProg/src/Indentifiers.java
@@ -0,0 +1,13 @@
+public class Indentifiers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int id = 5;
+		int num= 1;
+		int num1=5;
+		String stdname= "Tarun";
+		System.out.println(id + num);
+        System.out.println(id+num1);
+        System.out.println(id+stdname);
+	}
+}
