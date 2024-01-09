2840bb5bf293b57526c29e4d594c7e83e050d3d2
tarun <tarun@DESKTOP-83VNE9M>
2024-01-09 18:31:08 +0530

    secoundcommit

---
+++ FullStackProg/src/TypeCasting/Exe1.java
@@ -0,0 +1,14 @@
+package TypeCasting;
+
+public class Exe1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   byte a = 4;
+   double d = 45;
+   d =a;
+   System.out.println(a);
+   System.out.println(d);
+	}
+
+}
