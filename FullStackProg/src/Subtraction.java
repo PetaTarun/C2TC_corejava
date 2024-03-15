9f65dc7496521b229addaca8ad5122fac9fc96af
tarun <tarun@DESKTOP-83VNE9M>
2024-03-15 21:47:30 +0530

    commit2

---
+++ FullStackProg/src/Subtraction.java
@@ -0,0 +1,15 @@
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		int a=2;
+		int b=5;
+		int c=a-b;
+		System.out.println("Subtraction  is  "+c);
+
+	}
+
+}
