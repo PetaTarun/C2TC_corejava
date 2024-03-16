f4b2feac5792936ed30b5a8cf92103fc9ca9ec6a
tarun <tarun@DESKTOP-83VNE9M>
2024-03-16 08:31:19 +0530

    commit 3

---
+++ FullStackProg/src/Strings/CaseSensitive.java
@@ -0,0 +1,19 @@
+package Strings;
+
+public class CaseSensitive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String S1 ="JaVa";
+		String S2 ="JAVA";
+		if(S1==S2)
+		{
+			System.out.println("Reference are equal");
+		}
+		else
+		{
+			System.out.println("reference is unequal");
+		}
+	}
+
+}
