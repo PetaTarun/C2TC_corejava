5ed66bcd678a9bac7377e28ab49bcccd26dd0563
tarun <tarun@DESKTOP-83VNE9M>
2024-03-16 20:17:27 +0530

    commit 1

---
+++ FullStackProg/src/Strings/StringBuiltinMethods.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class StringBuiltinMethods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s ="Tarun";
+		System.out.println(s.toUpperCase());
+		System.out.println(s.toLowerCase());
+		System.out.println(s);// TARUN tarun tarun
+	}
+
+}
