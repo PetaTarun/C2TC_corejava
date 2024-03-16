87237cb9ee193df2eae1c348946fa4bfcfd97753
tarun <tarun@DESKTOP-83VNE9M>
2024-03-16 13:38:18 +0530

    commit 4

---
+++ FullStackProg/src/Strings/DuplicateAreNotAlllowed.java
@@ -0,0 +1,21 @@
+package Strings;
+
+public class DuplicateAreNotAlllowed {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String S1="java";
+		String S2="java";
+		if(S1==S2) //if(S1.equal(S2))
+		{
+			System.out.println("Reference is equal");//values are equal
+		}
+		else
+		{
+			System.out.println("reference is unequal");
+		}
+		
+	}
+}
+
+
