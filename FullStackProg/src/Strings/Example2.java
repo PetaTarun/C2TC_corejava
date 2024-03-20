cbe7f4da1b3c45b6b1771f2c11f215fa90fd2f2a
tarun <tarun@DESKTOP-83VNE9M>
2024-03-20 09:17:37 +0530

    first commit

---
+++ FullStackProg/src/Strings/Example2.java
@@ -0,0 +1,17 @@
+package Strings;
+import java.util.Scanner;
+public class Example2 {
+
+	 static String reverse(String s) {
+		// TODO Auto-generated method stub
+		String t="";
+		for(int i =s.length()-1;i>=0;i--) {
+			t=t+s.charAt(i);
+		}
+		return t;
+	}
+	 public static void main(String[] args) {
+		String s ="Tarun";
+		System.out.println(reverse(s));
+	}
+}
