089072de0a049dd7e53465111ca67324ef4a981f
Tarun <petatarun17@gmail.com>
2024-05-30 22:56:17 +0530

    Added example largest number among the two numbers

---
+++ FullStackProg/src/Arrays/LargestNoAmongArray.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class LargestNoAmongArray {
+	public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	int n =5;
+	System.out.println("enter the array elements :");
+	for (int i=0;i<n;i++) {
+		int arr[]=new int [5];
+		arr[0]=1;
+		arr[1]=i;
+		arr[2]=i;
+		arr[3]=i;
+		arr[4]=i;
+
+	}
+	
+	}
+}
