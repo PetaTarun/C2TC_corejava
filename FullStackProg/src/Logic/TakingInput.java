5e842f80a33263987b18a682a24f3be820b8c300
Tarun <petatarun17@gmail.com>
2024-05-30 22:55:29 +0530

    Taking Input from user

---
+++ FullStackProg/src/Logic/TakingInput.java
@@ -0,0 +1,9 @@
+package Logic;
+import java.util.Scanner;
+public class TakingInput {
+	public static void main (String[] args) {
+		 Scanner sc=new Scanner(System.in);
+	        String str=sc.nextLine();
+	        System.out.println("Hello, "+str);
+	}
+}
